2ec5a36a80f817c85cdc539c32077ee309e50868
Jackson <53550653+its-jackson@users.noreply.github.com>
2022-11-27 23:49:58 -0400

    Fix main layout logo position

--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -1,14 +1,19 @@
 package com.example.application.views;
 
 import com.example.application.security.SecurityService;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.RouterLink;
 
 public class MainLayout extends AppLayout {
@@ -21,8 +26,36 @@ public class MainLayout extends AppLayout {
         createDrawer();
     }
 
+    private Tabs getTabs() {
+        Tabs tabs = new Tabs();
+        tabs.add(
+                createTab(VaadinIcon.DASHBOARD, "Dashboard", DashboardView.class),
+                createTab(VaadinIcon.USER_HEART, "Customers", ListView.class)
+                //createTab(VaadinIcon.RECORDS, "Books"),
+                //createTab(VaadinIcon.CHART, "Analytics")
+        );
+        tabs.setOrientation(Tabs.Orientation.VERTICAL);
+        return tabs;
+    }
+
+    private Tab createTab(VaadinIcon viewIcon, String viewName, Class<? extends Component> navigationTarget) {
+        Icon icon = viewIcon.create();
+        icon.getStyle().set("box-sizing", "border-box")
+                .set("margin-inline-end", "var(--lumo-space-m)")
+                .set("margin-inline-start", "var(--lumo-space-xs)")
+                .set("padding", "var(--lumo-space-xs)");
+
+        RouterLink link = new RouterLink();
+        link.add(icon, new Span(viewName));
+        link.setRoute(navigationTarget);
+        link.setTabIndex(-1);
+        return new Tab(link);
+    }
+
     private void createHeader() {
         H1 logo = new H1("Mila Library");
+        logo.getStyle().set("font-size", "var(--lumo-font-size-l)")
+                .set("margin", "0");
         logo.addClassNames("text-l", "m-m");
 
         Button logout = new Button("Log out", e -> securityService.logout());
@@ -42,12 +75,6 @@ public class MainLayout extends AppLayout {
     }
 
     private void createDrawer() {
-        RouterLink listLink = new RouterLink("Customers", ListView.class);
-        listLink.setHighlightCondition(HighlightConditions.sameLocation());
-
-        addToDrawer(new VerticalLayout(
-                listLink,
-                new RouterLink("Dashboard", DashboardView.class)
-        ));
+        addToDrawer(new VerticalLayout(getTabs()));
     }
 }
